d358db0f11593da1e8f03761c2cfe03b2d6210c6
Hue <75088696+Hue-Jhan@users.noreply.github.com>
2025-11-09 18:40:19 +0100

    add hijack 2

---
+++ 2_Create_Hijack/hijak.h
@@ -0,0 +1,14 @@
+#pragma once
+#include <stdio.h>
+#include <Windows.h>
+#include <tlhelp32.h>
+#include <winbase.h>
+
+BOOL GetProcess(LPWSTR szProcessName, DWORD* dwProcessId, HANDLE* hProcess);
+
+BOOL GetThread(IN DWORD dwProcessId, OUT DWORD* dwThreadId, OUT HANDLE* hThread);
+
+BOOL HijackThread(IN HANDLE hThread, IN PVOID pAddress);
+
+VOID temp(VOID);
+BOOL CreateGetThread(HANDLE* hProcess, OUT HANDLE* hThread);
