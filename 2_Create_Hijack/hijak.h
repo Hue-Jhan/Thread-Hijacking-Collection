f02b29cd0555c7ea3f573c8ec7f8efce54aa7a50
Hue <75088696+Hue-Jhan@users.noreply.github.com>
2025-11-10 11:34:38 +0100

    Update hijak.h

--- 2_Create_Hijack/hijak.h
+++ 2_Create_Hijack/hijak.h
@@ -4,9 +4,9 @@
 #include <tlhelp32.h>
 #include <winbase.h>
 
-BOOL GetProcess(LPWSTR szProcessName, DWORD* dwProcessId, HANDLE* hProcess);
+BOOL GetProcess(LPWSTR processName, DWORD* pid, HANDLE* hProcess);
 
-BOOL GetThread(IN DWORD dwProcessId, OUT DWORD* dwThreadId, OUT HANDLE* hThread);
+BOOL GetThread(IN DWORD pid, OUT DWORD* tid, OUT HANDLE* hThread);
 
 BOOL HijackThread(IN HANDLE hThread, IN PVOID pAddress);
 
